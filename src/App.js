c41f2e7b1bfcbd00583e16d1fa1af200fdbf6bf0
nicksolony <nicksolony@gmail.com>
2021-07-31 19:22:42 -0400

    started adding header

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 
 //need to get logo from Anya to add it here
 
-import './App.css';
+// import './App.css';
 import './App.scss';
 function App() {
   return (
     <div className="App">
       <div className="header"></div>
-      <div className="content"></div>
+      <div className="content">asASDASDSfsd</div>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function Home(params) {
+function Home() {
     return (
         <div>
             <h1>Home Page</h1>
