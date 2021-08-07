c709db73856542140e2e29cbe5943ddb46349786
nicksolony <nicksolony@gmail.com>
2021-08-07 14:18:27 -0400

    Added Resume.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Resume from './components/Resume';
 
 
 //need to get logo from Anya to add it here
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Skills />
       <Projects />
+      <Resume />
 
 
     </div>
---
+++ src/components/Resume.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+class Resume extends Component {
+    render() {
+        return ( 
+            <div id='Resume'>
+                Resume would go here
+            </div>
+        )
+    }
+}
+
+export default Resume
+
