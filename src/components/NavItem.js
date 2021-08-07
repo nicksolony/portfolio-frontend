0f930d01237d99023ab63b9fc549895861e2a3fd
nicksolony <nicksolony@gmail.com>
2021-08-07 13:54:32 -0400

    added NavItem.js

---
+++ src/components/NavItem.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class NavItem extends Component {
+    render(){
+        return (
+            <div>
+                Home
+            </div>
+        )
+    }
+}
+
+export default NavItem;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react'
+import NavItem from './NavItem'
 
 class NavBar extends Component {
     render() {
         return ( 
             <div id='Navbar'>
             <ul>
-                <li>Home</li>
+                <li><NavItem /></li>
                 <li>About Me</li>
                 <li>Skills</li>
                 <li>Projects</li>
