e4eb798315c480a05eb4f3b9f826e50d2534b44b
nicksolony <nicksolony@gmail.com>
2021-08-20 19:32:50 -0400

    substitute embeded youtube for image

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,11 +5,7 @@ class ProjectItem extends Component {
 
     render(){
         
-        // const liveLink = () => {
-        //         if(this.props.item.live){
-        //           return <a href={this.props.item.live}><img src="../imgs/web.svg" alt="Live version" width="30px"/></a>
-        //         } 
-        //   }
+       
 
         return (
             
@@ -17,14 +13,12 @@ class ProjectItem extends Component {
             <div>
                 <h2>{this.props.item.name}</h2>
                 
-                <iframe width="300" height="300" src={this.props.item.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
+                {/* <iframe width="300" height="300" src={this.props.item.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe> */}
+                <img src={this.props.item.img} alt="" width="250" />
                 <p>{this.props.item.desc}</p>
                 {this.props.item.links.map((link)=> 
                 <ProjectLink link = {link}  />)}
-                {/* {liveLink()}
-                <a href={this.props.item.git}><img src="../imgs/git.png" alt="GitHub" width="30px"/></a>
-                <a href={this.props.item.youtube}><img src="../imgs/youtube.png" alt="Demo Recording" width="30px"/></a>
-                <a href={this.props.item.blog}><img src="../imgs/blog.png" alt="Blog Post" width="30px"/></a> */}
+              
                 
 
 
