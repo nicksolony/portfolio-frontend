c603ebef6523dfc6c0ad27c86e78b63451e8e2f6
nicksolony <nicksolony@gmail.com>
2021-08-20 18:15:41 -0400

    created extrated function to remove Live versionc link

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -4,6 +4,12 @@ class ProjectItem extends Component {
 
     render(){
         
+        const liveLink = () => {
+                if(this.props.item.live){
+                  return <a href={this.props.item.live}><img src="../imgs/web.svg" alt="Live version" width="30px"/></a>
+                } 
+          }
+
         return (
             
 
@@ -13,7 +19,7 @@ class ProjectItem extends Component {
                 <iframe width="300" height="300" src={this.props.item.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe>
                 <p>{this.props.item.desc}</p>
                 
-                <a href={this.props.item.live}><img src="../imgs/web.svg" alt="Live version" width="30px"/></a>
+                {liveLink()}
                 <a href={this.props.item.git}><img src="../imgs/git.png" alt="GitHub" width="30px"/></a>
                 <a href={this.props.item.youtube}><img src="../imgs/youtube.png" alt="Demo Recording" width="30px"/></a>
                 <a href={this.props.item.blog}><img src="../imgs/blog.png" alt="Blog Post" width="30px"/></a>
