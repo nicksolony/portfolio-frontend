1cd198e2970d8779864265855e44ac464e15b7de
nicksolony <nicksolony@gmail.com>
2021-10-12 22:57:23 -0400

    fixed gear in skills

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -4,6 +4,7 @@ class Skills extends Component {
     render() {
         return ( 
         <div id="Skills" className="component">
+            <img src="./imgs/gear_skills.png" alt="" />
             <h2>Technical Skills</h2>
             <ul>
                 {this.props.skills.map(skill => {
