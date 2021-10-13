ab5f7df2e829598c6c3f84aa048385b3ff4b1b39
nicksolony <nicksolony@gmail.com>
2021-10-12 23:14:16 -0400

    added left column to skills, and added margin

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -3,15 +3,19 @@ import React, {Component} from 'react'
 class Skills extends Component {
     render() {
         return ( 
-        <div id="Skills" className="component">
-            <img src="./imgs/gear_skills.png" alt="" />
-            <h2>Technical Skills</h2>
-            <ul>
-                {this.props.skills.map(skill => {
-                    return <li>{skill}</li>
-                //   <li>{skill}</li>  
-                })}
-            </ul>
+        <div id="Skills" className="component row">
+            <div className="left">
+                <img src="./imgs/gear_skills.png" alt="" />
+                <h2>Technical <br/> Skills</h2>
+            </div>
+            <div className="right">
+                <ul>
+                    {this.props.skills.map(skill => {
+                        return <li>{skill}</li>
+                    //   <li>{skill}</li>  
+                    })}
+                </ul>
+            </div>
         </div>
         )
     }
