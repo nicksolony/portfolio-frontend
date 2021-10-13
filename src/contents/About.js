4109723281f23ed80b95da9b93854e5168bb0ff2
nicksolony <nicksolony@gmail.com>
2021-10-12 23:26:58 -0400

    updated About by using rows and columns for formating

--- src/contents/About.js
+++ src/contents/About.js
@@ -3,19 +3,14 @@ import React, {Component} from 'react'
 class Skills extends Component {
     render() {
         return ( 
-        <div id="About" className="component">
-            <table>
-                <tr>
-                    <td width='35%'>
-                        <h2><img src="./imgs/photo.png" alt="Profile" className="photo" />
-                        About me</h2>
-                        <p>{this.props.content}</p>
-                    </td>
-                    <td>
-                    
-                    </td>
-                </tr>
-            </table>
+        <div id="About" className="component row">
+            <div className="left">
+            <h2><img src="./imgs/photo.png" alt="Profile" className="photo" />About me</h2>
+            <p>{this.props.content}</p>
+            </div>
+            <div className="right">
+                <img src="./imgs/BG3.gif" alt="" />
+            </div>
         </div>
         )
     }
