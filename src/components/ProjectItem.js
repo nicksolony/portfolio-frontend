4c7637e77c03fea95f93a128ab1c369afe341d4c
nicksolony <nicksolony@gmail.com>
2021-10-20 18:25:00 -0400

    fixed bg of projects section

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -10,11 +10,12 @@ class ProjectItem extends Component {
         return (
             
 
-            <div>
-                <h2>{this.props.item.name}</h2>
+            <div className="projectItem">
+                
                 
                 {/* <iframe width="300" height="300" src={this.props.item.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"></iframe> */}
-                <img className="projectItem" src={this.props.item.img} alt={this.props.item.name} />
+                <img className="projectImage" src={this.props.item.img} alt={this.props.item.name} />
+                <h2>{this.props.item.name}</h2>
                 <p>{this.props.item.desc}</p>
                 {this.props.item.links.map((link)=> 
                 <ProjectLink link = {link}  />)}
